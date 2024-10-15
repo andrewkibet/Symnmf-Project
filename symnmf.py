4e0b1fb253e591b8c0b2ef8d543dce67c353671c
Kibet <61360677+andrewkibet@users.noreply.github.com>
2024-10-16 02:32:45 +0300

    Update symnmf.py

--- symnmf.py
+++ symnmf.py
@@ -5,7 +5,7 @@ import mysymnmfsp as msm
 np.random.seed(0)
 
 def defX(filename):
-    return pd.read_csv(filename, header=None, delim_whitespace=True).astype(float).values.tolist()
+    return pd.read_csv(filename, header=None, sep='\s+').astype(float).values.tolist()
 
 # Hinitialization
 '''
